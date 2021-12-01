e57b98d8d5ce630c7912a5503bfcb61352bb5888
Tania Papandrea <TaniaPapandrea@gmail.com>
2021-12-01 11:27:07 -0800

    first stab at puzzle 1

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -1,1 +1,16 @@
-print('hello from Advent of Code day 1')
+### This returns one number too small
+
+file1 = open('input.txt', 'r')
+numbers = file1.readlines()
+
+count = 0
+l = len(numbers)
+
+for i in range(l):
+	num = numbers[i]
+	prev = numbers[i-1]
+	if (num > prev):
+		count += 1
+		print('{} increased from {}. count={}\n'.format(num, prev, count))
+	else:
+		print('{} decreased from {}. count={}\n'.format(num, prev, count))
