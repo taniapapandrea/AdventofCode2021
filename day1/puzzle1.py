5ed694d9faac988146802830074c3695ec0590a1
lbreede <lennart.breede@gmail.com>
2022-06-11 10:09:57 -0700

    with statement to avoid closing file and omit of r flag since its default

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -1,14 +1,14 @@
-file1 = open('input.txt', 'r')
-## Fun fact: if you don't cast to int(x), python thinks that "1003\n" < "985\n"
-numbers = [int(x) for x in file1.readlines()]
+with open("input.txt") as file1:
+    ## Fun fact: if you don't cast to int(x), python thinks that "1003\n" < "985\n"
+    numbers = [int(x) for x in file1.readlines()]
 
 count = 0
 l = len(numbers)
 
 for i in range(1, l):
-	num = numbers[i]
-	prev = numbers[i-1]
-	if (num > prev):
-		count += 1
+    num = numbers[i]
+    prev = numbers[i - 1]
+    if num > prev:
+        count += 1
 
 print(count)
